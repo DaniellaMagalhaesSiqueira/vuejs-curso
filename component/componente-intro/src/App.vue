9947bfbc1b1a536ea08f6a510ce35c610970223a
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-10-21 19:56:15 -0300

    projeto componente-intro

---
+++ component/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="#app">
+        <h1>Contadores</h1>
+        <scan>{{contador }}</scan>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
