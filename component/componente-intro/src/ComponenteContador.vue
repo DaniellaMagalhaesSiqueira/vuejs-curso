e3301d4954e1bcac439f2cf2856005a5700af880
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-10-22 11:24:39 -0300

    escopo do estilo

--- component/componente-intro/src/ComponenteContador.vue
+++ component/componente-intro/src/ComponenteContador.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="contador">
-
         <span>{{ contador }}</span>
         <button @click="adicionar">+</button>
         <button @click="subtrair">-</button>
@@ -25,13 +24,15 @@ export default {
 }
 </script>
 
-<style>
-    .contador{
+<style scoped>
+
+    span {
         border-bottom: 1px solid #CCC;
         height: 30px;
         padding: 5px 25px;
     }
-    .contador button{
+
+    button {
         height: 30px;
         width: 30px;
         border-radius: 15px;
