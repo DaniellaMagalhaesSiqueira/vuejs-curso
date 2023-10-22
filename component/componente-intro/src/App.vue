0ef9367bc8c711400ac1bfa75ae650921f4e57f5
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-10-22 10:45:26 -0300

    construindo componente

--- component/componente-intro/src/App.vue
+++ component/componente-intro/src/App.vue
@@ -1,18 +1,15 @@
 <template>
     <div id="#app">
         <h1>Contadores</h1>
-        <scan>{{contador }}</scan>
+        <app-contador></app-contador>
+        
     </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      contador: 0,
-    };
-  },
-};
+  
+}
 </script>
 
 <style>
